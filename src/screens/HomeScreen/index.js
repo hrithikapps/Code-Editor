0c1a6d8269b9806eeaa779eff7254c8e0eade099
hrithikapps <hrithikk2765@gmail.com>
2024-04-17 00:48:43 +0530

    feat: created left side component page

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,5 +1,22 @@
+import "./index.scss";
+
 export const HomeScreen = () => {
-    return (
-        <h1>Home Screen</h1>
-    );
-}
+  return (
+    <div className="home-container">
+      <div className="left-container">
+        <div className="items-container">
+          <img src="Logo.png" alt="Logo" />
+          <h1>CodeX</h1>
+          <h2>Code.Compile.Debug</h2>
+          <button>
+            <span className="material-icons">add</span>
+            <span>Create Playground</span>
+          </button>
+        </div>
+      </div>
+      <div className="Right-container">
+        <h1>Right-container</h1>
+      </div>
+    </div>
+  );
+};
